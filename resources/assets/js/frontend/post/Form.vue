281a8cb8ab9f5591be02c0dad58f81dda59343dc
overtrue <anzhengchao@gmail.com>
2017-01-18 22:49:27 +0800

    编辑器完成

--- resources/assets/js/frontend/post/Form.vue
+++ resources/assets/js/frontend/post/Form.vue
@@ -11,22 +11,53 @@
         <div class="picker">&plus; 设置封面</div>
       </div>
       <div class="post-title" placeholder="请输入标题" contenteditable="true"></div>
-      <div class="post-body" placeholder="请输入正文" contenteditable="true"></div>
+      <textarea class="post-body" id="post-body" placeholder="请输入内容"></textarea>
+    </div>
+    <div class="page-actions">
+      <a href="#" class="btn btn-outline-success" @click="handleSubmit()">发布</a>
+      <a href="#" class="btn btn-outline-secondary" @click="handleSubmit(false)">存为草稿</a>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'post-form',
-    data() {
-      return {
-        cover: null,
-        title: '',
-        body: ''
-      }
+import CodeMirror from "codemirror"
+
+require("./theme/yike.scss")
+require("codemirror/mode/gfm/gfm")
+require("codemirror/addon/display/placeholder")
+require("codemirror/keymap/sublime")
+
+export default {
+  name: 'post-form',
+  data() {
+    return {
+      cover: null,
+      title: '',
+      body: ''
+    }
+  },
+  mounted() {
+    var that = this
+
+    const editor = CodeMirror.fromTextArea(document.getElementById('post-body'), {
+      keyMap: "sublime",
+      mode:  "markdown",
+    });
+
+    editor.on('change', function(editor){
+        that.body = editor.getValue();
+      });
+
+    var pageActionsBox = document.querySelector('.page-actions-wrapper');
+    pageActionsBox.appendChild(document.querySelector('.page-actions'));
+  },
+  methods: {
+    handleSubmit(publish = true) {
+      console.log(publish, this.title, this.body);
     }
   }
+}
 </script>
 
 
@@ -64,5 +95,12 @@
 
   [placeholder]:empty:focus:before {
       content: "";
+  }
+
+
+  .post-new .CodeMirror {
+    font-family: inherit !important;
+    min-height: 300px;
+    color: black;
   }
 </style>
--- webpack.mix.js
+++ webpack.mix.js
@@ -32,5 +32,24 @@ mix.webpackConfig({
             path.resolve('node_modules'),
             path.resolve('./'),
         ]
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader'
+          },
+          {
+            test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+            loader: 'file-loader'
+          },
+          {
+            test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
+            loader: 'file-loader',
+            query: {
+              name: '[name].[ext]?[hash]'
+            }
+          }
+        ]
     }
 });
