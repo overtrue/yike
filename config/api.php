a47433e66edeecd43a9ecae966ca0e5a514512ee
jcc <changejian@gmail.com>
2017-02-09 20:37:03 +0800

    优化 Transformer 的引入

--- config/api.php
+++ config/api.php
@@ -32,10 +32,6 @@ return [
     */
 
     'transformers' => [
-        App\User::class => App\Transformers\UserTransformer::class,
-        App\Post::class => App\Transformers\PostTransformer::class,
-        App\Tag::class => App\Transformers\TagTransformer::class,
-        App\Role::class => App\Transformers\RoleTransformer::class,
-        App\Permission::class => App\Transformers\PermissionTransformer::class,
+        //
     ],
 ];
